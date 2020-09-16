bc027814a45327a965511c143417254c89234fed
belight <alejandroalcaraz@fp.uoc.edu>
2020-09-16 23:59:53 +0200

    ajustes

--- plantillas/casa.php
+++ plantillas/casa.php
@@ -1,10 +1,35 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Documento sin título</title>
-</head>
+<section class="section">
+	<div class="contenedor">
+		<h2>Esto que es??</h2>
+		<div class="presentacion clearfix">
+		
+			<div class="foto_perfil">
+				<img src="img/web_casa/retocadas/casa_4.jpg" alt="Foto Casa"/>
+			</div>
+			<div class="texto_presentacion">
+			<p>Esta es mi nueva casa, un proyecto de tranquilidad y esparcimiento</p>
+			</div>
+		</div>
+	
+	</div>
+	<h2>El exterior me lo estoy currando</h2>
+	<!--<div class="mapa" id="mapa"></div>-->
+	<div class="contenedor_mapa">
+	
+		<img src="img/web_casa/retocadas/casa_3.jpg" alt="Foto Casa"/>
+	</div>
+	<h2>La casa en mi linea</h2>
+</section>
 
-<body>
-</body>
-</html>
+<div class="contador parallax">
+	
+	<div class="contenedor cuenta">
+		
+		<ul class="resumen-evento clearfix">
+			<li><p id="dias" class="numero"></p>Días</li>
+			<li><p id="horas" class="numero"></p>Horas</li>
+			<li><p id="minutos" class="numero"></p>Minutos</li>
+			<li><p id="segundos" class="numero"></p>Segundos</li>
+		</ul>
+		</div>
+</div>
